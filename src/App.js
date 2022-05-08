95f69b1aced1e541507eab9888eb10a643d5f158
Afsana Ara Bristy <afsanaara928@gmail.com>
2022-05-08 19:25:12 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Components/Home/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+  
+      <Home></Home>
     </div>
   );
 }
