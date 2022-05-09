dc5d9b859f8a8dc27e031e481cd14835fb4051be
Afsana Ara Bristy <afsanaara928@gmail.com>
2022-05-09 23:27:00 +0600

    project done

--- src/Components/Inventory/Inventory.js
+++ src/Components/Inventory/Inventory.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import UseProducts from "../../Hooks/UseProducts/UseProducts";
+
 import Spinner from "../Spinner/Spinner";
 
 const Inventory = () => {
